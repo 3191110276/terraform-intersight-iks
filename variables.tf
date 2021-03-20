d4134b74a2f4f41c1bfda1475736093a45284597
Michael Maurer <3191110276@users.noreply.github.com>
2021-03-20 23:01:36 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,3 +4,41 @@ variable "org" {
   description = "Intersight organization which the cluster belongs to."
 }
 
+variable "cluster_name" {
+  type        = string
+  description = "The name of the Kubernetes cluster in Intersight. This property is used to name all policies and profiles."
+}
+
+variable "k8s_version" {
+  type        = string
+  default     = "1.18.12"
+  description = "The Kubernetes version that the created cluster will be based on."
+}
+
+variable "dns_servers" {
+  type        = list
+  description = "A list of DNS servers that will be used for configuring the Kubernetes cluster."
+}
+
+variable "ntp_servers" {
+  type        = list
+  description = "A list of NTP servers that will be used for configuring the Kubernetes cluster."
+}
+
+variable "timezone" {
+  type        = string
+  description = "A timezone that will be used for configuring the Kubernetes cluster."
+}
+
+variable "pod_network_cidr" {
+  type        = string
+  default     = "172.16.0.0/17"
+  description = "The CIDR used for the Kubernetes Pod network."
+}
+
+variable "service_cidr" {
+  type        = string
+  default     = "172.16.128.0/17"
+  description = "The CIDR used for the Kubernetes Service network."
+}
+
