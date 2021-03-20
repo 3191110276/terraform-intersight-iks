9ffea75d2a2259d86d98bd5c2b6a2e2cc6995211
Michael Maurer <3191110276@users.noreply.github.com>
2021-03-20 13:38:31 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
+############################################################
+# ADD THIRD PARTY PROVIDERS
+############################################################
+terraform {
+  required_providers {
+    intersight = {
+      source = "CiscoDevNet/intersight"
+      version = "1.0.0"
+    }
+  }
+}
 
