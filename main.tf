28e1e593870d5241784879326827eb46acd6e8cb
Michael Maurer <3191110276@users.noreply.github.com>
2021-04-17 18:22:30 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -163,9 +163,4 @@ resource "intersight_kubernetes_virtual_machine_infrastructure_provider" "k8s_in
     moid = data.intersight_asset_target.infra_target.results[0].moid
 
   }
-  
-  organization {
-    object_type = "organization.Organization"
-    moid        = data.intersight_organization_organization.organization.results[0].moid
-  }
 }
